05e39e6e471192433117ebdc0aefcbb8b5c999ac
hardikgarg2002 <hardikgarg3085@gmail.com>
2023-01-15 10:00:00 +0530

    setting up express server

---
+++ index.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from 'express';
+
+const port = process.env.PORT;
+
+app.get('/', (req: Request, res: Response) => {
+  res.setHeader('Content-Type', 'text/html');
+  const response = {
+    message: 'Welcome to the app Created by Hardik Garg',
+  };
+
+  res.send(response);
+});
+// default end point
+app.use('*', (req, res) => {
+  res.status(404).send({
+    error: 'AUTH404: Could not find the page requested by you',
+  });
+});
+
+app.listen(port, () => {
+  console.log('App listening on port', port);
+});
